01289800faa8af21b1eaaccf4258e3d55c42fb8f
alouilouai <louaialoui1993@gmail.com>
2025-06-26 00:24:15 +0100

    changes

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,5 @@
-import { SimpleGrid, keyframes } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
+import { keyframes } from '@emotion/react';
 import { useEffect } from "react";
 import Project from "../components/Projects/Project";
 import { projects } from "../../public/data/projects";
